ad13fc084a724ae081118cfd047e7ae912efadd8
Alex <alexabraham.esp@gmail.com>
2021-06-18 13:05:51 -0400

    start

--- sockets/socket.js
+++ sockets/socket.js
@@ -10,32 +10,32 @@ bands.addBand(new Band("Queen3"));
 bands.addBand(new Band("Queen4"));
 
 io.on("connection", (client) => {
-  client.emit("active-bands", bands.getBands());
+    client.emit("active-bands", bands.getBands());
 
-  client.on("disconnect", () => {
-    console.log("Cliente fuera");
-  });
-  client.on("mensaje", (payload) => {
-    io.emit("mensaje", { admin: "Nuevo mensaje" });
-  });
-  client.on("emitir-mensaje", (payload) => {
-    //  io.emit('nuevo-mensaje',payload)
-    client.broadcast.emit("emitir-mensaje", payload);
-  });
+    client.on("disconnect", () => {
+        console.log("Cliente fuera");
+    });
+    client.on("mensaje", (payload) => {
+        io.emit("mensaje", { admin: "Nuevo mensaje" });
+    });
+    client.on("emitir-mensaje", (payload) => {
+        //  io.emit('nuevo-mensaje',payload)
+        client.broadcast.emit("emitir-mensaje", payload);
+    });
 
 
-  client.on("vote-band", (payload) => {
-    bands.voteBand(payload.id);
-    io.emit("active-bands", bands.getBands());
-  });
-  client.on("add-band", (payload) => {
-    const newBand = new Band(payload.name)
-    bands.addBand(newBand);
-    io.emit("active-bands", bands.getBands());
-  });
+    client.on("vote-band", (payload) => {
+        bands.voteBand(payload.id);
+        io.emit("active-bands", bands.getBands());
+    });
+    client.on("add-band", (payload) => {
+        const newBand = new Band(payload.name)
+        bands.addBand(newBand);
+        io.emit("active-bands", bands.getBands());
+    });
 
-  client.on("delete-band", (payload) => {
-    bands.deteteBand(payload.id);
-    io.emit("active-bands", bands.getBands());
-  });
+    client.on("delete-band", (payload) => {
+        bands.deteteBand(payload.id);
+        io.emit("active-bands", bands.getBands());
+    });
 });
